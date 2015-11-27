3098653800bed3d07a1f5c367cd3a949f0ce5e61
pinahar <piyushnahar1991@gmail.com>
2015-11-27 00:21:37 -0800

    Restructure files

---
+++ src/uber_plot.py
@@ -0,0 +1,31 @@
+import cPickle as pickle
+from sklearn.cluster import KMeans as km
+from collections import defaultdict
+import pandas as pd
+
+all_data = pickle.load(open("all_data.p", "rb"))
+
+d = defaultdict(list)
+t = 2
+for row in all_data[t]:
+	d['lat'].append(float(row[2]))
+	d['long'].append(float(row[3]))
+
+df = pd.DataFrame(data=d)
+
+import matplotlib  
+import matplotlib.pyplot as plt  
+
+pd.options.display.mpl_style = 'default' #Better Styling  
+new_style = {'grid': False} #Remove grid  
+matplotlib.rc('axes', **new_style)  
+from matplotlib import rcParams  
+rcParams['figure.figsize'] = (17.5, 17) #Size of figure  
+rcParams['figure.dpi'] = 250
+
+P=df.plot(kind='scatter', x='long', y='lat',color='yellow',xlim=(-74.06,-73.77),ylim=(40.61, 40.91),s=.002,alpha=0.5)
+
+P.set_axis_bgcolor('black') #Background Color
+
+
+
