832513440c6b76832c3c0806ffa6dee13b2aaa79
pinahar <piyushnahar1991@gmail.com>
2015-11-27 21:14:44 -0800

    Add 21 outputs

--- src/plot_clusters.py
+++ src/plot_clusters.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 import numpy as np
 
-t = 1
-k = range(100,205,5)
+t = 21
+k = range(160,290,10)
 l_files = ['../outputs/output_{t_val}_{k_val}.txt'.format(t_val=t,k_val=i) for i in k]
 
 mse = []
