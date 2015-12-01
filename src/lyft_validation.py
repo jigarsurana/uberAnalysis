6c5a70832a925eeabeaefdefdf7ef557c348efe9
pinahar <piyushnahar1991@gmail.com>
2015-12-01 00:17:58 -0800

    Add Lyft validation

---
+++ src/lyft_validation.py
@@ -0,0 +1,20 @@
+import cPickle as pickle
+from sklearn.cluster import KMeans as km
+from geopy.distance import vincenty
+
+lyft = pickle.load(open("../data/all_data_lyft.p", "rb"))
+
+t = 21
+lyft_lat_long = []
+for row in lyft[t]:
+	lyft_lat_long.append([row[3], row[4]])
+
+pred = kmeans.predict(lyft_lat_long)
+
+pred_cluster_centers = [kmeans.cluster_centers_[i] for i in pred]
+
+error = [vincenty(lyft_lat_long[i],pred_cluster_centers[i]).miles for i in range(len(lyft_lat_long))]
+
+print "For t = {t_val}:".format(t_val = t)
+print "Min Error\t\tMax Error\t\tAvg Error".format()
+print "{min_e}\t{max_e}\t{avg_e}".format(min_e = min(error), max_e = max(error), avg_e = sum(error)/len(error))
